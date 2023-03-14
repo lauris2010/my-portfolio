0479653bffc02b346f0bbf608dbc547be356841c
lauris2010 <laurynas.martinkus2009@gmail.com>
2023-03-14 14:57:38 +0200

    Project section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 import { Experience, PageInfo, Project, Skill, Social } from "@/typings";
@@ -40,6 +41,9 @@ export default function Home({
       </section>
       <section id="skills" className="snap-start">
         <Skills skills={skills} />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
       </section>
     </div>
   );
