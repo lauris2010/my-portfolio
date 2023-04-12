27b5503db2857687b3b27d2fd97e248e6897cdc7
lauris2010 <laurynas.martinkus2009@gmail.com>
2023-04-12 17:34:34 +0300

    about fix

--- components/About.tsx
+++ components/About.tsx
@@ -40,7 +40,7 @@ function About({ pageInfo }: Props) {
         src={urlFor(pageInfo?.profilePic).url()}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
-      <div className="space-y-10 px-0 md:px-10 sm:text-center lg:text-left">
+      <div className="space-y-8 px-0 md:px-10 text-center sm:text-center lg:text-left">
         <h4 className="text-4xl font-semibold">
           Here is a{" "}
           <span className="underline underline-offset-4 decoration-[#63048B]/50">
